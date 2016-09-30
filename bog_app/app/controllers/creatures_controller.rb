1d430296d9fd1804bf2baa51cbe1343789dfff6e
Andrew Cordivari <andrewcord@William-Cordivaris-MacBook-Pro.local>
2016-09-30 16:05:38 -0700

    create and add are fully functional, views are functional

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -1,7 +1,34 @@
 class CreaturesController < ApplicationController
   def index
+  end
 
-  @creatures = Creature.all
+    @creatures = Creature.all
+
+
+    def new
+      @creature = Creature.new
+    end
+
+    def show
+
+      creature_id = params[:id]
+
+      @creature = Creature.find_by_id(creature_id)
+
+    end
+
+
+    def create
+
+      creature_params = params.require(:creature).permit(:name, :description)
+
+      creature = Creature.new(creature_params)
+
+      if creature.save
+        redirect_to creature_path(creature)
+
+    end
+
 
   end
 end
