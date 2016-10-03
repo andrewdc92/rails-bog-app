045e33458b1f7dddfe3e7950f83ed5e36fef2247
Andrew Cordivari <andrewcord@williamvarismbp.gateway.sonic.net>
2016-10-02 17:22:50 -0700

    all CRUD routes and features work, finished in 1:10

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -23,6 +23,17 @@ class CreaturesController < ApplicationController
     @creature = Creature.find_by_id(creature_id)
   end
 
+  def destroy
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id(creature_id)
+
+    creature.destroy
+
+    redirect_to creatures_path
+
+  end
+
   def update
     creature_id = params[:id]
 
