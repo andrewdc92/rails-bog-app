4bc4649e0f62e21f09f9a93444a13ce13652493c
Andrew Cordivari <andrewcord@williamvarismbp.gateway.sonic.net>
2016-10-02 17:10:38 -0700

    edit and update work

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -17,14 +17,36 @@ class CreaturesController < ApplicationController
     @creature = Creature.new
   end
 
+  def edit
+    creature_id = params[:id]
+
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id(creature_id)
+
+    creature.update_attributes(creature_params)
+
+    redirect_to creature_path(creature)
+  end
+
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
 
     creature = Creature.new(creature_params)
 
     if creature.save
       redirect_to creature_path(creature)
     end
+  end
+
+  private
+
+  def creature_params
+
+    params.require(:creature).permit(:name, :description)
   end
 
 end
